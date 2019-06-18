a91f7e35ec86725762fe85ae41a86831f03eb679
Jun Liang <illuminatemynight@gmail.com>
2019-06-18 20:33:27 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,3 +16,17 @@ function whileLoop(number) {
   }
   return "done"
 }
+
+var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+function doWhileLoop(integer) {
+  do{
+    console.log("I run once regardless.")
+  }
+  while (incrementVariable < integer)
+}
